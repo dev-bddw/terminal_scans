44ca1a912f6afc872d18d57c53498a503e738f04
Lance Knickerbocker <lance@bddw.com>
2022-07-14 14:29:14 -0400

    version 1.0

--- scans/urls.py
+++ scans/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
 from .views import (
-    connection_failed_hx,
+    button_test_hx,
     connection_test,
     scan_home_page,
     scan_hx,
-    send_failed_hx,
+    send_scans_hx,
 )
 
 app_name = "scans"
@@ -13,7 +13,7 @@ app_name = "scans"
 urlpatterns = [
     path("", view=scan_home_page, name="scan"),
     path("scan_hx", view=scan_hx, name="scan_hx"),
-    path("resend_scans_hx", view=send_failed_hx, name="resend_scans_hx"),
+    path("resend_scans_hx", view=send_scans_hx, name="resend_scans_hx"),
     path("internet-poll", view=connection_test, name="internet_poll"),
-    path("button-poll", view=connection_failed_hx, name="hx_button_poll"),
+    path("button-poll", view=button_test_hx, name="hx_button_poll"),
 ]
--- scans/views.py
+++ scans/views.py
@@ -11,34 +11,30 @@ from .models import Scan
 
 def connection_test(request):
 
-    internet_status = 1 if is_connected("google.com") else 0
-
     return render(
-        request, "partials/internet.html", {"internet_status": internet_status}
+        request, "partials/internet.html", {"is_connected": is_connected("google.com")}
     )
 
 
-def connection_failed_hx(request):
+def button_test_hx(request):
     scans = Scan.objects.all().order_by("-time_scan")
 
-    has_failed_scans = False
+    has_non_uploaded = False
 
     for scan in scans:
-        if scan.time_upload is None and scan.scan_id is None:
-            has_failed_scans = True
+        if scan.scan_id is None:
+            has_non_uploaded = True
             break
-        else:
-            has_failed_scans = False
-
-    internet_status = 1 if is_connected("google.com") else 0
 
     return render(
         request,
         "partials/resend_failed_button.html",
         {
             "scans": scans,
-            "internet_status": internet_status,
-            "has_failed_scans": has_failed_scans,
+            "is_connected": is_connected("google.com"),
+            "scan_button_on": True
+            if (has_non_uploaded is True and is_connected("google.com"))
+            else False,
         },
     )
 
@@ -47,24 +43,13 @@ def scan_home_page(request):
 
     scans = Scan.objects.all().order_by("-time_scan")
 
-    has_failed_scans = False
-
-    for scan in scans:
-        if scan.time_upload is None and scan.scan_id is None:
-            has_failed_scans = True
-            break
-        else:
-            has_failed_scans = False
-
-    internet_status = 1 if is_connected("google.com") else 0
-
     return render(
         request,
         "scan.html",
         {
             "scans": scans,
-            "internet_status": internet_status,
-            "has_failed_scans": has_failed_scans,
+            "is_connected": False,
+            "scan_button_on": False,
         },
     )
 
@@ -72,65 +57,20 @@ def scan_home_page(request):
 def scan_hx(request):
 
     sku = request.POST.get("sku")
-    internet_status = 1
 
     scans = Scan.objects.all().order_by("-time_scan")
 
-    has_failed_scans = [
-        x for x in scans if (x.time_upload is None) and (x.scan_id is None)
-    ]
-
-    if sku != "":
+    if sku != "" and sku[0] != " ":
 
         new_scan = Scan(sku=sku, location=settings.LOCATION_CODE)
         new_scan.save()
-
-        if is_connected("google.com"):
-
-            try:
-
-                app_key = settings.APP_KEY
-                scan_data = {
-                    "sku": new_scan.sku,
-                    "time_scan": new_scan.time_scan.strftime("%Y-%m-%dT%H:%M:%S.%f%z"),
-                    "location": new_scan.location,
-                }
-
-                data_json = json.dumps(scan_data)
-
-                headers = CaseInsensitiveDict()
-                headers["Accept"] = "application/json"
-                headers["Content-type"] = "application/json"
-                headers["Authorization"] = "Token {}".format(app_key)
-
-                response = requests.post(
-                    "https://bddwscans.com/endpoint/", data=data_json, headers=headers
-                )
-
-                if response.status_code == 200:
-                    print(response.json())
-                    new_scan.scan_id = response.json()["scan_id"]
-                    new_scan.time_upload = response.json()["time_upload"]
-                    new_scan.save()
-
-                if response.status_code == 403:
-                    print("API KEY ERROR")
-
-            except requests.exceptions.ConnectionError:
-                internet_status = 0
-                print("There was a connection problem. Saving the scan locally.")
-                pass
-        else:
-            internet_status = 0
-            print("There was a connection problem")
 
         return render(
             request,
             "partials/hx_table.html",
             {
                 "scans": Scan.objects.all().order_by("-time_scan"),
-                "internet_status": internet_status,
-                "has_failed_scans": has_failed_scans != [],
+                "scan_button_on": False,
             },
         )
 
@@ -140,51 +80,46 @@ def scan_hx(request):
             "partials/hx_table.html",
             {
                 "scans": scans,
-                "internet_status": internet_status,
-                "has_failed_scans": has_failed_scans != [],
+                "scan_button_on": False,
             },
         )
 
 
-def send_failed_hx(request):
+def send_scans_hx(request):
 
-    internet_status = 1
+    internet_status = 0
 
     for scan in Scan.objects.filter(time_upload=None):
+
         if is_connected("google.com"):
-            try:
-                app_key = settings.APP_KEY
-                scan_data = {
-                    "sku": scan.sku,
-                    "time_scan": scan.time_scan.strftime("%Y-%m-%dT%H:%M:%S.%f%z"),
-                    "location": scan.location,
-                }
-                data_json = json.dumps(scan_data)
 
-                headers = CaseInsensitiveDict()
-                headers["Accept"] = "application/json"
-                headers["Content-type"] = "application/json"
-                headers["Authorization"] = "Token {}".format(app_key)
+            app_key = settings.APP_KEY
+            scan_data = {
+                "sku": scan.sku,
+                "time_scan": scan.time_scan.strftime("%Y-%m-%dT%H:%M:%S.%f%z"),
+                "location": scan.location,
+            }
+            data_json = json.dumps(scan_data)
 
-                response = requests.post(
-                    "https://bddwscans.com/endpoint/", data=data_json, headers=headers
-                )
+            headers = CaseInsensitiveDict()
+            headers["Accept"] = "application/json"
+            headers["Content-type"] = "application/json"
+            headers["Authorization"] = "Token {}".format(app_key)
 
-                if response.status_code == 200:
-                    print(response.json())
-                    scan.scan_id = response.json()["scan_id"]
-                    scan.time_upload = response.json()["time_upload"]
-                    scan.save()
+            response = requests.post(
+                "https://bddwscans.com/endpoint/", data=data_json, headers=headers
+            )
 
-                if response.status_code == 403:
-                    print("API KEY ERROR")
+            if response.status_code == 200:
+                print(response.json())
+                scan.scan_id = response.json()["scan_id"]
+                scan.time_upload = response.json()["time_upload"]
+                scan.save()
 
-            except requests.exceptions.ConnectionError:
-                print("There was a connection problem. Saving the scan locally.")
-                pass
-        else:
-            internet_status = 0
-            print("There was a connection problem. Saving the scan locally.")
+            if response.status_code == 403:
+                print("API KEY ERROR")
+
+            internet_status = 1
 
     return render(
         request,
